e0a3486e0ac575f7439094fcc6a8b5d7d2cdd2bc
Alex Mejicanos <alexmejicanos@outlook.com>
2019-08-04 14:12:20 -0600

    Adding CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 /** Express App */
 const app = express();
@@ -12,6 +13,7 @@ const UsersTypesController = require('./controllers/UsersTypes');
 const UsersController = require('./controllers/Users');
 
 app.use(express.json());
+app.use(cors());
 app.use(new HealthCheckController(express.Router()).router);
 app.use(new UsersTypesController(express.Router()).router);
 app.use(new UsersController(express.Router()).router);
